f959e61c048947bf6dfcc0afa4d651a00afef232
Mariel <marielk.mya@gmail.com>
2018-06-22 08:53:43 -0400

    cambios en data, jueves por la tarde

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -1,7 +1,7 @@
 	let users = [];
 	let progress = [];
 	let courses = []; 
-	const progressText = [];
+
  //function obtener datos users
 	//llamando al JSON con fetch
 	const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
@@ -12,7 +12,8 @@
 	})	
 	const usersFull = data => {
 	data.forEach(elem => {
-		users.push(elem.id);
+		users = data;
+		//console.log(users[1].id);
 	 	}) 
 	}
  //function obtener datos progress
@@ -21,33 +22,59 @@
 	fetch(progressJSON) 
 	.then(response => response.json())
 	.then(data => {   
-		progress.push(data);
-		progressText.push(JSON.stringify(data));
-		
+		progress = Object.entries(data);
+		//console.log(progress[0][1].intro.units);
 	})	
-	
- //function obtener datos courses	
-	
-	//for (let i= 0; i <progress.length; i++){
-		 
-	
-	
-	
-		//console.log(progressObject);
-	
-	
-//console.log(progressText); 
-//console.log(users); 
-//console.log(progress);
-//console.log(courses); 
-
-	
-  
 
 //funcion  que debe contener toda la info
 window.computeUsersStats = (users, progress, courses) => {
+	//objeto constructor para crear usuarios
+	class User {
+	  constructor(id, name, cohort){
+		this.id = id;
+		this.name = name;
+		this.cohort = cohort;
+		}
+	
+	}
+	//creando alumna de prueba 
+	let alumna1 = new User(users[0].id, users[0].name, users[0].signupCohort);
+	//asignando propiedad stats
+	alumna1.stats = "student";
+	//asignando cursos a la alumna
+	alumna1.stats.courses = progress.find(element => element[0] === users[0].id);
+	//obteniendo progreso general de la alumna
+	alumna1.stats.percent = progress[0][1].intro.percent;
 
+	//desglosando informacion de JSON progress de la unidad 1
+	let a = progress[0][1].intro.units; // unidades
+	let b = Object.entries(a); // array de unidades
+	let c = b[0][1].parts; // partes //desde aca se puede replicar para otras unidades
+	let d = Object.entries(c); // array de partes
+	let e = d[0][1].type // tipo de la parte 1, de la unidad 1
+	let f = []; //variable para mostrar solo los tipo reads. 
+	let q = []; //variable para mostrar solo los tipo quizz. 
+	let j = []; //variable para mostrar solo los tipo excercises. 
+	
+	//ciclo para sacar solo los tipo read
+	d.forEach((element)=> {
+		if (element[1].type === "read") f.push(element)});
 
+	//ciclo for para sacar solo los tipo quizz
+	d.forEach((element)=> {
+		if (element[1].type === "quizz") q.push(element)});
+
+	//ciclo for para sacar solo los tipo excersises
+	d.forEach((element)=> {
+		if (element[1].type === "excercises") j.push(element)});
+
+	/*
+	//obteniendo estadistica de ejercicios
+	alumna1.stats.exercises = progress.find(element === "practice")
+	//obteniendo estadistica de quizzes
+	alumna1.stats.quizzes = 
+	//obteniendo estadistica de reads
+	alumna1.stats.reads =  */
 }
 	
 
@@ -68,7 +95,18 @@ window.processCohortData = (options) => {
 
 
 /*
+//function obtener datos courses	
+//function findCourses(element) {
+//return element = "00hJv4mzvqM3D9kBy3dfxoJyFV82";}
 
+//console.log(users.findIndex(findCourses));
+	//for (let i= 0; i <progress.length; i++){
+		 
+	
+	
+	
+		//console.log(progressObject);
+//console.log(progressText);
 let a = progressText.map((element) => {
 	courses.push(element[0]);
 });
